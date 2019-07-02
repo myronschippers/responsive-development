f9a5c007f30042f91c9d6f883f6ca645f5163565
Myron <dubtwee@gmail.com>
2019-07-01 22:01:37 -0500

    footer styling update for the application

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStateToProps from '../../redux/mapStateToProps';
 
+import './Footer.css';
+
 class Template extends Component {
     render() {
         return(
-            <footer>
-                Footer Content
+            <footer className="footer">
+                &copy;Prime Digital Academy
             </footer>
         )
     }
