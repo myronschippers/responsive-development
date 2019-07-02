9bcd28cb8ad2c17e9d87bfbc2a95717c9ef482d5
Myron <dubtwee@gmail.com>
2019-07-01 21:38:22 -0500

    styling navigation and application header

--- src/components/AppToppers/AppTopper.js
+++ src/components/AppToppers/AppTopper.js
@@ -3,29 +3,67 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStateToProps from '../../redux/mapStateToProps';
 
+// STYLING
+import './AppTopper.css';
+
 class AppTopper extends Component {
+    state = {
+        isNavOpen: true,
+    };
+
+    onToggleMobileNav = (event) => {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen,
+        });
+    }
+
     render() {
+        let navPaneStyles = 'nav-pane';
+
+        if (this.state.isNavOpen) {
+            navPaneStyles += ' nav-pane_isOpen';
+        }
+
         return(
             <header className="appTopper">
                 <div className="appTopper-identity">
-                    <h1>Responsive</h1>
+                    <div className="logo">
+                        <svg className="logo-img" viewBox="0 0 512 512">
+                            <use xlinkHref="/images/responsive-icon.svg#responsive"></use>
+                        </svg>
+                        <h1 className="logo-text">Responsive</h1>
+                    </div>
                 </div>
                 <div className="appTopper-actions">
                     <nav className="nav">
-                        <button className="nav-mobileAct" onClick={this.onToggleMobileNav}>
+                        <button
+                            className="nav-mobileAct"
+                            onClick={this.onToggleMobileNav}
+                        >
                             Navigation
                         </button>
-                        <ul className="nav-list">
-                            <li>
-                                <a href="/">About</a>
-                            </li>
-                            <li>
-                                <a href="/">Store</a>
-                            </li>
-                            <li>
-                                <a href="/">Contact</a>
-                            </li>
-                        </ul>
+
+                        <div className={navPaneStyles}>
+                            <div className="nav-pane-hd">
+                                <button
+                                    className="btnClose"
+                                    onClick={this.onToggleMobileNav}
+                                >
+                                    Close
+                                </button>
+                            </div>
+                            <ul className="nav-pane-list">
+                                <li>
+                                    <a className="navLink" href="/">About</a>
+                                </li>
+                                <li>
+                                    <a className="navLink" href="/">Store</a>
+                                </li>
+                                <li>
+                                    <a className="navLink" href="/">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
                     </nav>
                 </div>
             </header>
